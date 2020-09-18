f405b9becce9895bb903b1a88aeeba496aa201c4
aniruddh-mukherjee <aniruddha.xccentric@gmail.com>
2020-09-18 06:31:54 +0530

    SpotifyWebApi Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getTokenFromUrl } from "./API/spotify";
 import LoginPage from "./Pages/Login/LoginPage";
+import Player from "./Pages/Player/Player";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <LoginPage />
-    </div>
-  );
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+    }
+  }, []);
+  return <div className="app">{token ? <Player /> : <LoginPage />}</div>;
 }
 
 export default App;
